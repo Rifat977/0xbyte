a9658e4bd19cae9e98a8f349f79477c1f53f53e9
rifat977 <itscrifat5147@gmail.com>
2023-08-15 20:26:29 +0600

    animation

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@fortawesome/fontawesome-free/css/all.css',
+    '@/assets/global.css'
+
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
